82a7460e8d28d2b9f18069ddaae51a4d3593a814
Robert <rmetcalf9@googlemail.com>
2025-05-16 21:26:28 +0100

    Deploy latest PWA build

---
+++ assets/axios.6fb44d8e.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.21c047ff.js";import{a}from"./axios.703e2798.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
